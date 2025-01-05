7c147c31fe4f18b20759b245a77232287020845f
engelryan <ryaneengel@gmail.com>
2025-01-05 14:59:49 -0300

    First Commit

---
+++ basics/OperadorTernario.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class OperadorTernario {
+
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        String response = (number>=5) ? "O número é maior que 5" : "O número é menor que 5";
+
+        System.out.println(response);
+    }
+}
